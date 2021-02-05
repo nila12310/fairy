b74e3c04345c1ca05685e0cb6a399b85821c1d8b
nila12310 <75618531+nila12310@users.noreply.github.com>
2021-02-05 12:21:55 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ const Body = Matter.Body;
 function preload()
 {
 	starImg = loadImage("images/star.png");
-	fairyImg = loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
+	fairyImg = loadAnimation("images/fairy.png","images/fairyImage2.png");
 	bgImg = loadImage("images/starnight.png");
 	
 
